12d2339d7234c8220a6602535eeedc645d8de574
Sebastian Nae <naesebastian96@gmail.com>
2023-09-16 21:56:18 +0300

    Added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './shared-library/components/navbar/navbar.component';
+import { CardComponent } from './shared-library/components/card/card.component';
+import { CarouselComponent } from './shared-library/components/carousel/carousel.component';
+import { AutocompleteComponent } from './shared-library/components/autocomplete/autocomplete.component';
+import { HomeComponent } from './smart-components/home/home.component';
+import { LandingPageComponent } from './smart-components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    CarouselComponent,
+    AutocompleteComponent,
+    HomeComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
