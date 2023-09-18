29a3cc20dd04c6b462276a84c26804d4ca5fe5cd
Sebastian Nae <naesebastian96@gmail.com>
2023-09-18 11:34:12 +0300

    Added boxicons and few global CSS changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -29,6 +29,7 @@ import { ImageSectionComponent } from './shared-library/components/image-section
     BrowserAnimationsModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
