e9a2ef0dcf77902cd246102f96a8eb73b5578eb3
Sebastian Nae <naesebastian96@gmail.com>
2023-10-29 19:58:12 +0200

    Fixed navbar transparancy change onscroll

--- src/app/shared-library/components/navbar/navbar.component.ts
+++ src/app/shared-library/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent, throttleTime, map } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  @ViewChild('navbarWrapper') navbarWrapper!: ElementRef<HTMLElement>;
 
+  ngAfterViewInit() {
+    fromEvent(window, 'scroll').pipe(
+      throttleTime(10), // limit the frequency of the scroll event
+      map(() => window.scrollY)
+    ).subscribe(scrollY => {
+      const maxOpacity = 0.9;
+      const opacity = Math.min(scrollY / 100, maxOpacity); 
+      this.navbarWrapper.nativeElement.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+    });
+  }
 }
